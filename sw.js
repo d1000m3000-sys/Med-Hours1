7541e74ff304d3cbb9a82d97526633fc56f6bafa
d1000m3000-sys <d1000m3000@gmail.com>
2026-07-29 23:14:37 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,52 +1,55 @@
-const CACHE_NAME = "med-hours-v2";
+const CACHE_NAME = 'med-hours-v2';
 
 const ASSETS = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./app.js",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
   );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
+self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
+    caches.keys().then((keys) => {
+      return Promise.all(
         keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
-    )
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      );
+    })
   );
   self.clients.claim();
 });
 
-self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
 
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return (
-        response ||
-        fetch(event.request).then(networkResponse => {
-          const clone = networkResponse.clone();
+    caches.match(event.request).then((cached) => {
+      if (cached) {
+        return cached;
+      }
 
-          caches.open(CACHE_NAME).then(cache => {
-            cache.put(event.request, clone);
-          });
+      return fetch(event.request).then((response) => {
+        const clone = response.clone();
 
-          return networkResponse;
-        })
-      );
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, clone);
+        });
+
+        return response;
+      });
     })
   );
 });
